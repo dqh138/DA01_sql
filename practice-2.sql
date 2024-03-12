8dbb8eae498741458e7d801c848bdb42a21e77e5
dqh138 <110094259+dqh138@users.noreply.github.com>
2024-03-13 03:08:05 +0700

    Create practice-2.sql

---
+++ practice-2.sql
@@ -0,0 +1,41 @@
+--ex1
+select distinct city
+from station
+where id%2 = 0;
+--ex2
+select count(city) - count(distinct city)
+from station;
+--ex3
+select ceiling (avg(salary) - avg(REPLACE(salary,0, '')))
+from employees;
+--ex4
+SELECT ROUND(CAST(SUM(item_count*order_occurrences)/SUM(order_occurrences) as decimal), 1) as mean
+FROM items_per_order;
+--ex5
+select candidate_id
+FROM candidates
+where  skill in('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING Count(skill)=3
+ORDER BY candidate_id ASC;
+--ex6
+SELECT user_id, 
+Date(max(post_date))- date(min(post_date)) as days_between
+FROM posts
+WHERE post_date >= '2021-01-01' AND post_date <= '2022-01-01'
+GROUP BY user_id
+HAVING COUNT(user_id) > 1;
+--ex7
+SELECT card_name,
+Max(issued_amount)-min(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
+--ex8
+SELECT manufacturer, 
+count(drug) as drug_count,
+SUM(cogs - total_sales) as total_loss
+from pharmacy_sales
+WHERE cogs>total_sales
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
