7ab301e2b448d145aa458ce1747074cd5ca50253
dqh138 <110094259+dqh138@users.noreply.github.com>
2024-05-15 01:27:43 +0700

    Update project1.sql

--- project1.sql
+++ project1.sql
@@ -14,4 +14,27 @@ or ORDERLINENUMBER is NULL
 or SALES is NULL
 or ORDERDATE is NULL
 
+--3
+Alter table public.sales_dataset_rfm_prj
+add column first_name varchar(250),
+add column last_name varchar(250);
+
+UPDATE public.sales_dataset_rfm_prj
+SET 
+first_name = INITCAP(SUBSTRING(contactfullname FROM 1 FOR POSITION('-' IN contactfullname) - 1)),
+last_name = INITCAP(SUBSTRING(contactfullname FROM POSITION('-' IN contactfullname) + 1));
+
+--4
+ALTER TABLE public.sales_dataset_rfm_prj
+ADD COLUMN QTR_ID INT,
+ADD COLUMN MONTH_ID INT,
+ADD COLUMN YEAR_ID INT;
+
+UPDATE public.sales_dataset_rfm_prj
+SET 
+    QTR_ID = EXTRACT(QUARTER FROM ORDERDATE),
+    MONTH_ID = EXTRACT(MONTH FROM ORDERDATE),
+    YEAR_ID = EXTRACT(YEAR FROM ORDERDATE);
+
+--5
 
